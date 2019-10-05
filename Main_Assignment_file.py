203390cccf3b87bc6209bdb42fcdc26719940f2c
Ajay <ajay.patel@mail.mcgill.ca>
2019-10-05 15:07:58 -0400

    frequency dict

--- Main_Assignment_file.py
+++ Main_Assignment_file.py
@@ -1,6 +1,8 @@
 import numpy as np
 from numpy import genfromtxt
 import pandas as pd
+from collections import Counter
+
 #a = genfromtxt('reddit_test.csv', delimiter=',')
 #b = genfromtxt('reddit_test.csv', delimiter=',')
 reddit_test = pd.read_csv('reddit_test.csv', sep=',',header=None)
@@ -10,7 +12,24 @@ comment_id=reddit_train.iloc[1:,0].values.astype(int)
 comment=reddit_train.iloc[1:,1].values.astype(str)
 subreddit=reddit_train.iloc[1:,2].values.astype(str)
 word_list=list()
-for i  in comment:
-    word_row=i.split(" ")
+for i in comment:
+    word_row = i.split(" ")
     for j in word_row:
         word_list.append(j)
+
+
+
+d = {}
+for x in word_list:
+    if (len(x) <= 4): 
+        if x.lower() not in d.keys():
+            d[x.lower()] = 1
+        else: 
+            d[x.lower()] += 1
+
+k = Counter(d)
+high = k.most_common(50)
+
+for i in high:
+    print(i[0], " :", i[1]," ")
+
