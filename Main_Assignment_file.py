ee0377bdb07cc5daaea9ce811018e64caa18b4cd
LiamBlount <50602230+LiamBlount@users.noreply.github.com>
2019-10-05 14:58:49 -0400

    Update Main_Assignment_file.py

--- Main_Assignment_file.py
+++ Main_Assignment_file.py
@@ -5,11 +5,15 @@ import pandas as pd
 #b = genfromtxt('reddit_test.csv', delimiter=',')
 reddit_test = pd.read_csv('reddit_test.csv', sep=',',header=None)
 reddit_train = pd.read_csv('reddit_train.csv', sep=',',header=None)
-
+#separates train data into 3 categories
+#id0-69999
 comment_id=reddit_train.iloc[1:,0].values.astype(int)
+#the comment
 comment=reddit_train.iloc[1:,1].values.astype(str)
+#which subreddit its form
 subreddit=reddit_train.iloc[1:,2].values.astype(str)
 word_list=list()
+#gets the words from the comments
 for i  in comment:
     word_row=i.split(" ")
     for j in word_row:
