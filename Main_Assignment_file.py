597801d26c3e1dafce147a86ab3843f62d8a0893
LiamBlount <50602230+LiamBlount@users.noreply.github.com>
2019-10-05 15:50:55 -0400

    Update Main_Assignment_file.py

--- Main_Assignment_file.py
+++ Main_Assignment_file.py
@@ -15,14 +15,18 @@ comment=reddit_train.iloc[1:,1].values.astype(str)
 #which subreddit its form
 subreddit=reddit_train.iloc[1:,2].values.astype(str)
 word_list=list()
-#gets the words from the comments
+what_comment_is_from=list()
+counter=0
 for i  in comment:
     word_row=i.split(" ")
     for j in word_row:
         word_list.append(j)
-
-
-
+        what_comment_is_from.append(counter)
+    counter+=1
+#removes non alphanumeric character from the strings
+for i in range(len(word_list)):
+    word_list[i] = re.sub('[^0-9a-zA-Z]+', '', word_list[i])
+    
 d = {}
 for x in word_list:
     if (len(x) <= 4): 
