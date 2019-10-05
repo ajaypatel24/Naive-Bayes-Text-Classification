3aa214f263062653cb309d131bc80bdec4a4fe79
LiamBlount <50602230+LiamBlount@users.noreply.github.com>
2019-10-05 14:47:28 -0400

    Update Main_Assignment_file.py

--- Main_Assignment_file.py
+++ Main_Assignment_file.py
@@ -5,12 +5,12 @@ import pandas as pd
 #b = genfromtxt('reddit_test.csv', delimiter=',')
 reddit_test = pd.read_csv('reddit_test.csv', sep=',',header=None)
 reddit_train = pd.read_csv('reddit_train.csv', sep=',',header=None)
+
+comment_id=reddit_train.iloc[1:,0].values.astype(int)
+comment=reddit_train.iloc[1:,1].values.astype(str)
+subreddit=reddit_train.iloc[1:,2].values.astype(str)
 word_list=list()
 for i  in comment:
     word_row=i.split(" ")
     for j in word_row:
         word_list.append(j)
-
-comment_id=reddit_train.iloc[1:,0].values.astype(int)
-comment=reddit_train.iloc[1:,1].values.astype(str)
-subreddit=reddit_train.iloc[1:,2].values.astype(str)
