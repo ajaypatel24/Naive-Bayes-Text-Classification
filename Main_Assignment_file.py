0c9bdc5285a0716f2f263b3c07e83202f5dbad91
Ajay <ajay.patel@mail.mcgill.ca>
2019-10-06 14:59:39 -0400

    added dictionary of comment_id and subreddit
    also deleted 50 most common words from wordlist

--- Main_Assignment_file.py
+++ Main_Assignment_file.py
@@ -2,7 +2,7 @@ import numpy as np
 from numpy import genfromtxt
 import pandas as pd
 from collections import Counter
-import re
+import re 
 
 #a = genfromtxt('reddit_test.csv', delimiter=',')
 #b = genfromtxt('reddit_test.csv', delimiter=',')
@@ -18,13 +18,19 @@ subreddit=reddit_train.iloc[1:,2].values.astype(str)
 word_list=list()
 what_comment_is_from=list()
 counter=0
-for i  in comment:
+for i in comment:
     word_row=i.split(" ")
     for j in word_row:
         word_list.append(j)
-        what_comment_is_from.append(counter)
+        what_comment_is_from.append(counter) 
     counter+=1
 #removes non alphanumeric character from the strings
+
+
+print(len(comment))
+#print(len(comment) != len(set(comment))) #if true there are duplicates in the list
+print(len(subreddit))
+'''
 for i in range(len(word_list)):
     word_list[i] = re.sub('[^0-9a-zA-Z]+', '', word_list[i])
     
@@ -39,19 +45,46 @@ for x in word_list:
 
 
 k = Counter(d)
-high = k.most_common(100)
+high = k.most_common(50)
+for i in high:
+    print(i[0], " ", i[1])
 
 transdict = {}
 for y in d:
     transdict[y] = ''
 
-print(transdict)
+#print(transdict)
 
 #print(string.maketrans(transdict))
+def removeFromList(the_list, val):
+   return [value for value in the_list if value != val]
+
+print(len(word_list))
+
+for i in high:
+    print(i[0])
+    word_list = removeFromList(word_list, i[0])
 
 
+print(len(word_list))
+'''
+'''
+delete 50 most common from word_list() and what_comment_is_this_from
+dict to link comment to subreddit
+get counter for word for each subreddit
+'''
+
+CommentToSubreddit = {}
+
+for x, y in zip(comment_id, subreddit): #instead of using comment, use comment_id since duplicates exist
+    CommentToSubreddit[x] = y
+    
 
 
+print(len(CommentToSubreddit)) #currently there are 842 comments in the list which are duplicates of other comments
+
+
+print(CommentToSubreddit[60000])
 
 #print(len(d))
 
