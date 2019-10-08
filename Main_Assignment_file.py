9cf93a9f8562c4493ca5f491786f893991e7de28
LiamBlount <50602230+LiamBlount@users.noreply.github.com>
2019-10-08 19:41:19 -0400

    Update Main_Assignment_file.py

--- Main_Assignment_file.py
+++ Main_Assignment_file.py
@@ -16,23 +16,25 @@ comment=reddit_train.iloc[1:,1].values.astype(str)
 #which subreddit its form
 subreddit=reddit_train.iloc[1:,2].values.astype(str)
 word_list=list()
-what_comment_is_from=list()
+what_is_the_last_word=list()
+index=list()
 counter=0
 for i in comment:
     word_row=i.split(" ")
     for j in word_row:
         word_list.append(j)
-        what_comment_is_from.append(counter) 
-    counter+=1
+        counter+=1
+    what_is_the_last_word.append(j)
+    index.append(counter-1)
 #removes non alphanumeric character from the strings
-
+a=index[5]
 print(len(comment))
 #print(len(comment) != len(set(comment))) #if true there are duplicates in the list, already verified that this is the case
 print(len(subreddit))
 
 for i in range(len(word_list)):
     word_list[i] = re.sub('[^0-9a-zA-Z]+', '', word_list[i])
-    
+'''    
 d = {}
 
 for x in word_list:
@@ -45,22 +47,9 @@ for x in word_list:
 
 #orders dictionary entries from highest to lowest 
 k = Counter(d)
-high = k.most_common(2)
-####new###
-'''
-goes through the list of the last words in the comments 
-if the last word is a word the we are going to delete
-we move the index back one and the penultimate word becomes the new last word
-if that word is still a word to be deleted we keep moving the index back one till we get one that isnt
-i.e if comment is 'the cat is cool' 
-what_is_the_last_word ='cool'
-index=4
-word_to_del=cool
-=> what_is_last_word='is'
-index='3'
-Warning I havent put in an error testor if we delete a whole comment i.e say a comment is 'No'
-and we want to delete No it will go onto the prev comment
-'''
+high = k.most_common(2)'''
+high=['']
+[x.lower for x in word_list]
 for word_to_del in high:
     for word,i in zip(what_is_the_last_word,range(len(what_is_the_last_word))):
         if word==word_to_del:#if the last word is one of the words were going to delete change it to be the one before
@@ -70,25 +59,28 @@ for word_to_del in high:
            while what_is_the_last_word[i]==word_to_del:#could potentially delte a whole comment 
                what_is_the_last_word[i]=word_list[index[i]-1]
                index[i]=index[i]-1
-###end new##             
+               
+               
 def removeFromList(the_list, val): #list operation to remove all occurences of a word very quickly
    return [value for value in the_list if value != val]
 
+    
+            
 def removeFromComment(CommentList, val): #removes corresponding removed word from the array of quote ID's (VERY SLOW, BOTTLENECKING EVERYTHING)
     return [value for value in CommentList if word_list[CommentList.index(value)] != val]
 
 #print("start: ", len(word_list))
 #print("start what: ", len(what_comment_is_from))
 
-[x.lower for x in word_list] #lowercase everything
+ #lowercase everything
 '''
 for y in high: #iterates over created top x words
     res_list = list(filter(lambda x: word_list[x] == y[0], range(len(word_list)))) #uses lambdas to keep track of all removed indices to apply them to the what_comment_is_from array
     word_list = removeFromList(word_list, y[0]) 
     for x in sorted(res_list, reverse=True): #the run time of this is ridiculous even with 2 words, need to change it somehow
         del what_comment_is_from[x]
-        
-'''
+ '''       
+
 
 
 
@@ -111,18 +103,3 @@ for x, y in zip(comment_id, subreddit): #instead of using comment, use comment_i
 print(len(CommentToSubreddit)) 
 
 #print(len(d))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
