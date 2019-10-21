7e442e02143eb7804549c29e8ae1d1a2b1faf80e
larakollokian <lara.kollokian@gmail.com>
2019-10-20 22:06:16 -0400

    cleaned bayes

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,5 +1,5 @@
-import numpy as np
-import pandas as pd
+import pandas as pd 
+import numpy as np 
 from collections import defaultdict
 import re
 from Processing import DataPreprocess
@@ -8,91 +8,89 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 
 def preprocess_string(str_arg):
-    cleaned_str=re.sub('[^a-z\s]+',' ',str_arg,flags=re.IGNORECASE) #every char except alphabets is replaced
-    cleaned_str=re.sub('(\s+)',' ',cleaned_str) #multiple spaces are replaced by single space
-    cleaned_str=cleaned_str.lower() #converting the cleaned string to lower case
+    clean_comment = re.sub('[^a-z\s]+',' ',str_arg,flags = re.IGNORECASE) 
+    clean_comment = re.sub('(\s+)',' ',clean_comment)
+    clean_comment = clean_comment.lower()
     
-    return cleaned_str # returning the preprocessed string 
+    return clean_comment
 
 class NaiveBayes:
-
+    
     #takes number of categories (subreddits) as input
     def __init__(self, subreddits):
         self.classes = subreddits
-
+        
     #creating a dictionary with words and their frequencies in each subreddit
     def word_frequencies(self, comment, label_index):
-        if isinstance(comment,np.ndarray): comment=comment[0]
+        if isinstance(comment,np.ndarray): comment = comment[0]
         for word in comment.split():
-            self.dict[label_index][word]+=1 #storing frequencies for each word in each category in dictionary
-
+            self.frequency_dict[label_index][word] += 1 #storing frequencies for each word in each category in dictionary
+            
     #takes training data and subreddit names as input, computes frequencies
     def fit(self, dataset, subreddit_names):
+    
         self.examples = dataset
         self.labels = subreddit_names
-        self.dict = np.array([defaultdict(lambda:0) for index in range(self.classes.shape[0])])
-
+        self.frequency_dict = np.array([defaultdict(lambda:0) for index in range(self.classes.shape[0])])
+            
         #construct word frequencies for each subreddit (training)
-        for subreddit_index, subreddit in enumerate(self.classes):
-            comments_from_subreddit = self.examples[self.labels == subreddit]
-            cleaned_examples=[preprocess_string(comment_subr) for comment_subr in comments_from_subreddit]
-            cleaned_examples = pd.DataFrame(data = comments_from_subreddit)
-            np.apply_along_axis(self.word_frequencies, 1, cleaned_examples, subreddit_index)
+        for subreddit_index,subreddit in enumerate(self.classes):
+            comments_from_subreddit = self.examples[self.labels == subreddit] #looking at examples from single subreddit at a time
+            
+            cleaned_comments = [preprocess_string(comment_subr) for comment_subr in comments_from_subreddit]
+            cleaned_comments = pd.DataFrame(data = cleaned_comments)
+            
+            #populating word frequency dictionary
+            np.apply_along_axis(self.word_frequencies,1,cleaned_comments,subreddit_index)
 
         #pre-calculating important values
-        prob_subreddits=np.empty(self.classes.shape[0])
-        all_words=[]
-        word_counts_in_subreddit=np.empty(self.classes.shape[0])
+        subreddit_probabilities = np.empty(self.classes.shape[0])
+        all_words_in_dataset = []
+        word_counts_in_subreddit = np.empty(self.classes.shape[0])
 
-        for subreddit_index, subreddit in enumerate(self.classes):
-            #Calculating probability of each subreddit ( p(c) )
-            prob_subreddits[subreddit_index]=np.sum(self.labels == subreddit)/float(self.labels.shape[0]) 
-            count = list(self.dict[subreddit_index].values())
-            word_counts_in_subreddit[subreddit_index] = np.sum(np.array(count)) + 1 # vocab is remaining to be added                              
-            all_words+=self.dict[subreddit_index].keys()
-                                                     
-        #calculating vocabulary
-        self.vocabulary=np.unique(np.array(all_words))
-        self.vocabulary_length=self.vocabulary.shape[0]
+        for subreddit_index,subreddit in enumerate(self.classes):
+            #Calculating prior probability of each subreddit ( p(c) )
+            subreddit_probabilities[subreddit_index] = np.sum(self.labels == subreddit) / float(self.labels.shape[0]) 
+            #Calculating word counts within the subreddits 
+            count = list(self.frequency_dict[subreddit_index].values())
+            word_counts_in_subreddit[subreddit_index] = np.sum(np.array(list(self.frequency_dict[subreddit_index].values()))) + 1
+            #adding to total word count                               
+            all_words_in_dataset += self.frequency_dict[subreddit_index].keys()
                                   
-        #computing normalizing terms                                      
-        denominators = np.array([word_counts_in_subreddit[subreddit_index] + self.vocabulary_length + 1 for subreddit_index, subreddit in enumerate(self.classes)]) 
-
-        #storing all precomputed values as tuples to be more clean
-        self.subreddit_values = [(self.dict[subreddit_index], prob_subreddits[subreddit_index], denominators[subreddit_index]) for subreddit_index, subreddit in enumerate(self.classes)]                               
-        self.subreddit_values = np.array(self.subreddit_values) 
-    
-    #gets probability of comment coming from each subreddit
-    def get_comment_probability(self, comment):                                                                     
-        subreddit_likelihoods = np.zeros(self.classes.shape[0]) #to store probability w.r.t each class
-        for subreddit_index, subreddit in enumerate(self.classes): 
-            for word in comment.split(): #split the test example and get p of each test word                                                     
-                word_counts = self.subreddit_values[subreddit_index][0].get(word, 0) + 1                          
-                word_prob = word_counts/float(self.subreddit_values[subreddit_index][2])                              
-                subreddit_likelihoods[subreddit_index] += np.log(word_prob)
-                                              
-        conditional_prob = np.empty(self.classes.shape[0])
-        for subreddit_index, subreddit in enumerate(self.classes):
-            conditional_prob[subreddit_index] = subreddit_likelihoods[subreddit_index] + np.log(self.subreddit_values[subreddit_index][1])                                  
-      
-        return conditional_prob
-
-    #takes a set of input points X as input and outputs predictions y hat
-    def predict(self, test_set):
-        y_hat = [] #array to hold predictions
-        for comment in test_set:
-            cleaned_example=preprocess_string(comment)
-            conditional_prob = self.get_comment_probability(cleaned_example)
-            y_hat.append(self.classes[np.argmax(conditional_prob)])
+        #computing normalizing terms                              
+        denominators = np.array([word_counts_in_subreddit[subreddit_index] + 2 for subreddit_index,subreddit in enumerate(self.classes)])                                                                          
         
-        return np.array(y_hat)
-
+        #storing all precomputed values as tuples to be more clean
+        self.subreddit_values = [(self.frequency_dict[subreddit_index],subreddit_probabilities[subreddit_index],denominators[subreddit_index]) for subreddit_index,subreddit in enumerate(self.classes)]                               
+        self.subreddit_values = np.array(self.subreddit_values)                                 
+                                              
+    #gets probability of comment coming from each subreddit                                          
+    def get_comment_probability(self,comment):
+        subreddit_likelihoods = np.zeros(self.classes.shape[0])
+        #calculating prior probabilities for each subreddit
+        for subreddit_index, subreddit in enumerate(self.classes):            
+            for word in comment.split():                            
+                word_counts = self.subreddit_values[subreddit_index][0].get(word,0) + 1                           
+                word_likelihood = word_counts / float(self.subreddit_values[subreddit_index][2])                              
+                subreddit_likelihoods[subreddit_index] += np.log(word_likelihood) #log likelihood
+                                              
+        posterior_prob = np.empty(self.classes.shape[0])
+        for subreddit_index,subreddit in enumerate(self.classes):
+            posterior_prob[subreddit_index] = subreddit_likelihoods[subreddit_index]+np.log(self.subreddit_values[subreddit_index][1])                                  
+        return posterior_prob
+    
+    #takes a set of input points X as input and outputs predictions y hat
+    def predict(self, X): 
+        y_hat = [] #holds predictions
+        for comment in X:                                  
+            cleaned_comment = preprocess_string(comment)                                  
+            posterior_prob = self.get_comment_probability(cleaned_comment) 
+            y_hat.append(self.classes[np.argmax(posterior_prob)])
+        return np.array(y_hat) 
+    
     #takes true labels and target labels and outputs accuracy
     def evaluate_acc(self, test_labels, predictions):
-        return np.sum(predictions==test_labels)/float(test_labels.shape[0])
-
-    #script to run k-fold cross validation
-    #def cross_validation(k):
+        return np.sum(predictions == test_labels) / float(test_labels.shape[0])
 
 ###########################################################################
 
@@ -102,7 +100,7 @@ reddit_test = pd.read_csv('reddit_test.csv')
 
 preprocess = DataPreprocess(reddit_train, reddit_test)
 
-TrainX, TestX, TrainY, TestY = train_test_split(preprocess.comment, preprocess.subreddit, test_size=0.05, random_state=4)
+TrainX, TestX, TrainY, TestY = train_test_split(preprocess.comment, preprocess.subreddit, test_size = 0.05, random_state = 4)
 RealTestX = preprocess.TestComment
 
 n_bayes = NaiveBayes(np.unique(TrainY))
